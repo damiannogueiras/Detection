a844b10f59501975cb1abc426438a7af5b19ecdf
Damian Nogueiras <damiannogueiras@gmail.com>
2020-02-16 18:06:32 +0100

    Threshold B&N

--- testCamera.py
+++ testCamera.py
@@ -1,12 +1,23 @@
 import numpy as np
 import cv2
 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
 cv2.startWindowThread()
 cap = cv2.VideoCapture(1)
 
 while(True):
     # reading the frame
     ret, frame = cap.read()
+    # turn to greyscale:
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # apply threshold. all pixels with a level larger than 150 are shown in white. the others are shown in black:
+    ret, frame = cv2.threshold(frame, 150, 255, cv2.THRESH_BINARY)
+
+
     # displaying the frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
